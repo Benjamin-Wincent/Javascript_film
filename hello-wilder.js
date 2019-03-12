9517c0957fc1853f9ca6ca0f26e0c8cfd9f9b511
Benjamin-Wincent <47710647+Benjamin-Wincent@users.noreply.github.com>
2019-03-12 10:27:44 +0100

    Add files via upload

---
+++ hello-wilder.js
@@ -0,0 +1,10 @@
+/* hello-wilder.js */
+
+// Define some variables
+const nameFilm = 'Snatch';
+const dateSortie = '2000';
+const nameRealisateur = 'Guy Ritchie';
+const message = nameFilm + " réalisé par " + nameRealisateur + " est sorti en " + dateSortie
+
+// Display a popup
+alert(message);
